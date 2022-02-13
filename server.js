ddcd0fdb31a1ea13ed7056f4001cab3069c9e5b3
devyetii <ebrahim.gomaa.hg@gmail.com>
2022-02-13 10:46:14 +0200

    Trying to get ip and port

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const WebSocket = require('ws')
 
 const wss = new WebSocket.Server({ port: process.env.PORT || 8080 })
 
-wss.on('connection', ws => {
+wss.on('connection', (ws, req) => {
   ws.on('message', message => {
-    const ip = ws._socket.remoteAddress;
-    const port = ws._socket.remotePort;
-    console.log(`Received message => ${message}`)
-    ws.send(`ECHO ${message} from ${ip}:${port}`);
+    const ip = req.socket.remoteAddress;
+    const port = req.socket.remotePort;
+    const forwarded = req.headers['x-forwarded-for'];
+    console.log(`Received message => ${message}`);
+    ws.send(`ECHO ${message}. remoteAddr = ${ip}, remotePort = ${port}, x-forwarded-for = ${forwarded}`);
   });
 });
