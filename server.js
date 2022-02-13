5f7aa88e092c835ec4f0dd4eee4a0f2bf0ac946f
devyetii <ebrahim.gomaa.hg@gmail.com>
2022-02-13 21:06:26 +0200

    Trying to get remote port behind proxy (2)

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ wss.on('connection', (ws, req) => {
     const ip = req.socket.remoteAddress;
     const port = req.socket.remotePort;
     const forwarded = req.headers['x-forwarded-for'];
+    const forwardedHost = req.headers['x-forwarded-host'];
     const forwardedPort = req.headers['x-forwarded-port'];
     console.log(`Received message => ${message}`);
-    ws.send(`ECHO ${message}. remoteAddr = ${ip}, remotePort = ${port}, x-forwarded-for = ${forwarded}, x-forwarded-port = ${forwardedPort}`);
+    ws.send(`ECHO ${message}. remoteAddr = ${ip}, remotePort = ${port}, x-forwarded-for = ${forwarded}, x-forwarded-host = ${forwardedHost}, x-forwarded-port = ${forwardedPort}`);
   });
 });
