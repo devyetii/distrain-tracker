ec716a901e434b8fa1c6cdda9a6bed3949075b52
devyetii <ebrahim.gomaa.hg@gmail.com>
2022-02-13 10:31:38 +0200

    Add ip and port to reponse

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const wss = new WebSocket.Server({ port: process.env.PORT || 8080 })
 
 wss.on('connection', ws => {
   ws.on('message', message => {
+    const ip = ws._socket.remoteAddress;
+    const port = ws._socket.remotePort;
     console.log(`Received message => ${message}`)
-    ws.send(`ECHO ${message}`)
-  })
-})
+    ws.send(`ECHO ${message} from ${ip}:${port}`);
+  });
+});
