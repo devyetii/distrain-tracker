6c07b29aea9e46d932efab5d579f26193126e800
devyetii <ebrahim.gomaa.hg@gmail.com>
2022-03-24 14:37:30 +0200

    Remove and ignore dist

--- dist/helpers.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sendMessage = void 0;
-function sendMessage(socket, type, data) {
-    console.log("Hot");
-    const message = { type, data };
-    socket.send(JSON.stringify(message));
-}
-exports.sendMessage = sendMessage;
