f975bd85b2aa55c524a596a0b9074f4f39896199
HmanA6399 <ebrahim.gomaa.hg@gmail.com>
2022-05-10 01:09:36 +0200

    Minor fix

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,7 +1,6 @@
 import { WebSocket } from "ws";
 
 export function sendMessage(socket: WebSocket, type: string, data: string) {
-    console.log("Hot")
     const message = { type, data };
     socket.send(JSON.stringify(message));
 }
