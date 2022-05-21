8f48e936b6fde4c7ed95d54806b994012f7c5de7
HmanA6399 <ebrahim.gomaa.hg@gmail.com>
2022-05-21 18:16:34 +0200

    Add dummy s3 file

--- s3.js
+++ s3.js
@@ -1,9 +1,29 @@
-const AWS = require('aws-sdk');
 
-const S3 = new AWS.S3();
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import fetch from 'node-fetch';
 
-// const uuid = require('uuid').v4;
 const uuid = "17f45f43-4a6d-4e59-8c8c-7582df58fec1";
 
-console.log(uuid())
+const s3 = new S3Client({ region: 'us-west-2' })
 
+const gamed = async () => {
+    try {
+        const command = new PutObjectCommand({
+            Bucket: "distrain-tracker",
+            Key: `t${uuid}/m.json`,
+        });
+    
+        const signedUrl = await getSignedUrl(s3, command, { expiresIn: 3600 });
+        console.log(signedUrl);
+    
+        // const response = await fetch(signedUrl, {method: 'PUT', body: JSON.stringify({ hi: "gamed" })});
+        // console.log(
+        //     `\nResponse returned by signed URL: ${await response.text()}\n`
+        //     );
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+gamed()
