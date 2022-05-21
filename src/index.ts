5674119f9d8a15e99b21bd0bb90c04245aa74eb5
HmanA6399 <ebrahim.gomaa.hg@gmail.com>
2022-05-21 18:19:27 +0200

    Fix index error

--- src/index.ts
+++ src/index.ts
@@ -96,47 +96,6 @@ wss.on('listening', () =>
 	console.log(`Listening on port ${port}`);
 });
 
-wss.on('connection', async (ws, req) =>
-{
-	let deviceId = <string>req.headers[ 'x-device-id' ];
-	let deviceAddress = <string>req.headers[ 'x-device-address' ];
-
-	if (deviceId === "" || deviceId === null || deviceId === undefined)
-	{
-		deviceId = uuid();
-		console.log("New device");
-		await deviceRepo.createDevice({
-			id: deviceId,
-			status: "idle",
-			last_login: new Date(Date.now()),
-			address: deviceAddress || "",
-		});
-		ws.send(JSON.stringify({ type: 'deviceId', data: deviceId }));
-	} else
-	{
-		console.log("Device found before");
-		deviceRepo.updateDevice({
-			id: deviceId,
-			status: 'idle',
-			last_login: new Date(Date.now()),
-		});
-	}
-
-	deviceRepo.setStatus(deviceId, "idle");
-	deviceRepo.setSocket(deviceId, ws);
-
-	ws.addEventListener('message', (message: MessageEvent) =>
-	{
-		const msgInstance = MessageFactory.createMessage(ws, <string>(message.data))
-		msgInstance?.handle();
-	});
-
-	ws.addEventListener('close', ({ code }) =>
-	{
-		console.log(`Socket ${deviceId} closing ... Code`, code);
-		deviceRepo.disconnectDevice(deviceId);
-	});
-
 wss.on('connection', async (ws, req) => {
   let deviceId = <string>req.headers['x-device-id'];
   let deviceAddress = <string>req.headers['x-device-address'];
@@ -236,4 +195,4 @@ async function schedule() {
       }
     }
   }
-}}
+}
