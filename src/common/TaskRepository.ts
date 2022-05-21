d3495969451bbb803bff1ae04ab1564e0d448001
HmanA6399 <ebrahim.gomaa.hg@gmail.com>
2022-05-21 17:58:13 +0200

    Add function to get task with min devices

--- src/common/TaskRepository.ts
+++ src/common/TaskRepository.ts
@@ -1,13 +1,32 @@
 import { DBClient } from './db';
+import { Singleton } from './Singleton';
 export interface Task {
     id: string;
     devices_count: number;
     params: string;
     status: "new" | "ongoing" | "succeeded" | "failed";
-    model_url?: string;
     chunck_url?: string;
+    data_type: string;
+    data_type_params: string;
 }
 
+@Singleton
 export class TaskRepository {
     private dbClient: DBClient = new DBClient();
+
+    public async getMinTask() {
+        const session = this.dbClient.getSession();
+        let task: Task | null = null;
+        try {
+            const res = await session.readTransaction(tx => 
+                tx.run(`MATCH (t:TASK) RETURN t ORDER BY t.devices_count LIMIT 1`)
+            );
+            const rec = res.records[0].get('t');
+            task = { ...rec.properties }
+        } catch (err) {
+            console.error(err)
+        }
+
+        return task;
+    }
 }
