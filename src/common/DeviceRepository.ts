997d3b30dce60ec5265ab58d7bc4e4e7db0f3e84
tarek99samy <tarek.saad99@eng-st.cu.edu.eg>
2022-05-22 02:37:39 +0200

    finalize the scheduler flow

--- src/common/DeviceRepository.ts
+++ src/common/DeviceRepository.ts
@@ -1,168 +1,188 @@
-import { DBClient } from './db';
-import { WebSocket } from 'ws';
+import { DBClient } from "./db";
+import { WebSocket } from "ws";
 import { Singleton } from "./Singleton";
-import { createClient, RedisClientType } from 'redis'
-import neo4j from 'neo4j-driver';
+import { createClient, RedisClientType } from "redis";
+import neo4j from "neo4j-driver";
 
 type DeviceStatus = "disconnected" | "idle" | "busy";
 
-interface Device
-{
-	id: string;
-	address: string;
-	status: DeviceStatus;
-	last_login: Date;
+interface Device {
+  id: string;
+  address: string;
+  status: DeviceStatus;
+  last_login: Date;
 }
 
-
 @Singleton
-export class DeviceRepository
-{
-	private client: RedisClientType;
-	private dbClient: DBClient;
-	private socketStore: Map<string, WebSocket | null>;
-	public devices: Device[];
+export class DeviceRepository {
+  private client: RedisClientType;
+  private dbClient: DBClient;
+  private socketStore: Map<string, WebSocket | null>;
+  public devices: Device[];
 
-	constructor(url: string | undefined)
-	{
-		this.client = createClient({ url });
-		this.socketStore = new Map<string, WebSocket | null>();
-		this.dbClient = new DBClient();
-		this.devices = [];
-		this.client.on('error', (err) =>
-		{
-			console.error("Redis Client Error: ", err);
-		});
+  constructor(url: string | undefined) {
+    this.client = createClient({ url });
+    this.socketStore = new Map<string, WebSocket | null>();
+    this.dbClient = new DBClient();
+    this.devices = [];
+    this.client.on("error", (err) => {
+      console.error("Redis Client Error: ", err);
+    });
 
-		this._connect();
-	}
+    this._connect();
+  }
 
-	private async _connect()
-	{
-		try
-		{
-			await this.client.connect();
-		} catch (err)
-		{
-			console.error("Couldn't connect to redis: ", err);
-		}
-	}
+  private async _connect() {
+    try {
+      await this.client.connect();
+    } catch (err) {
+      console.error("Couldn't connect to redis: ", err);
+    }
+  }
 
-	private _makeEditString(object: Object, varName: string): string
-	{
-		return Object.entries(object)
-			.filter(([ k, v ]) => k !== 'id' && v !== undefined)
-			.reduce((acc, [ k, _ ], i, a) =>
-			{
-				if (acc === '') acc += 'SET ';
-				acc += `${varName}.${k} = $${k}${(i < a.length - 1) ? ',' : ''} `;
-				return acc;
-			}, '');
-	}
+  private _makeEditString(object: Object, varName: string): string {
+    return Object.entries(object)
+      .filter(([k, v]) => k !== "id" && v !== undefined)
+      .reduce((acc, [k, _], i, a) => {
+        if (acc === "") acc += "SET ";
+        acc += `${varName}.${k} = $${k}${i < a.length - 1 ? "," : ""} `;
+        return acc;
+      }, "");
+  }
 
-	public async createDevice(device: Device)
-	{
-		const session = this.dbClient.getSession();
-		try
-		{
-			await session.writeTransaction(tx =>
-				tx.run(
-					"CREATE (newDevice:DEVICE) SET newDevice.id = $id, newDevice.address = $address, newDevice.status = $status, newDevice.last_login = $last_login",
-					{
-						...device,
-						last_login: neo4j.types.DateTime.fromStandardDate(device.last_login),
-					}
-				)
-			);
-			this.devices.push(device);
-		} catch (err)
-		{
-			console.error("Neo4J store error", err);
-		} finally
-		{
-			session.close();
-		}
-	}
+  public async createDevice(device: Device) {
+    const session = this.dbClient.getSession();
+    try {
+      await session.writeTransaction((tx) =>
+        tx.run(
+          "CREATE (newDevice:DEVICE) SET newDevice.id = $id, newDevice.address = $address, newDevice.status = $status, newDevice.last_login = $last_login",
+          {
+            ...device,
+            last_login: neo4j.types.DateTime.fromStandardDate(device.last_login),
+          }
+        )
+      );
+      this.devices.push(device);
+    } catch (err) {
+      console.error("Neo4J store error", err);
+    } finally {
+      // session.close();
+    }
+  }
 
-	public async updateDevice(device: Partial<Device>)
-	{
-		const session = this.dbClient.getSession();
-		const lastLoginDate = device.last_login ?? new Date();
-		try
-		{
-			const res = await session.writeTransaction(tx =>
-				tx.run(`MATCH (d:DEVICE {id: $id}) ${this._makeEditString(device, 'd')} RETURN d`, { ...device, last_login: neo4j.types.DateTime.fromStandardDate(lastLoginDate) })
-			);
-			console.log(`Found ${res.records.length}`);
-			console.log("Device status and login updated");
-		} catch (err)
-		{
-			console.error(`Neo4j Update Error: `, err);
-		} finally
-		{
-			session.close();
-		}
-	}
+  public async updateDevice(device: Partial<Device>) {
+    const session = this.dbClient.getSession();
+    const lastLoginDate = device.last_login ?? new Date();
+    try {
+      const res = await session.writeTransaction((tx) =>
+        tx.run(`MATCH (d:DEVICE {id: $id}) ${this._makeEditString(device, "d")} RETURN d`, {
+          ...device,
+          last_login: neo4j.types.DateTime.fromStandardDate(lastLoginDate),
+        })
+      );
+      console.log(`Found ${res.records.length}`);
+      console.log("Device status and login updated");
+    } catch (err) {
+      console.error(`Neo4j Update Error: `, err);
+    } finally {
+      // session.close();
+    }
+  }
 
-	public async setStatus(key: string, value: DeviceStatus)
-	{
-		try
-		{
+  public async connectDeviceToTask(deviceId: string, taskId: string, number: number) {
+    const session = this.dbClient.getSession();
+    try {
+      await session.writeTransaction((tx) =>
+        tx.run(`MATCH (d:DEVICE {id: $deviceId}), (t:TASK {id: $taskId}) CREATE (d)-[r:WORKS_ON {number: $number}]->(t)`, { deviceId, taskId, number })
+      );
+    } catch (err) {
+      console.error("Neo4j connect error", err);
+    } finally {
+      // session.close();
+    }
+  }
 
-			await this.client.set(`${key}`, `${value}`);
-		} catch (err)
-		{
-			console.error(`Couldn't set value of ${key} to redis: `, err);
-		}
-	}
+  public async disconnectDeviceFromTask(deviceId: string, taskId: string) {
+    const session = this.dbClient.getSession();
+    try {
+      await session.writeTransaction((tx) => tx.run(`MATCH (d:DEVICE {id: $deviceId})-[r:WORKS_ON]->(t:TASK {id: $taskId}) DELETE r`, { deviceId, taskId }));
+    } catch (err) {
+      console.error("Neo4j disconnect error", err);
+    } finally {
+      // session.close();
+    }
+  }
 
-	public async getStatus(key: string): Promise<DeviceStatus | null>
-	{
-		try
-		{
-			return await this.client.get(key) as DeviceStatus;
-		} catch (err)
-		{
-			console.error(`Couldn't get value of ${key} from redis: `, err);
-			return null
-		}
-	}
+  public async makeDevicesMesh(sourceId: string, otherDevicesId: string[], taskId: string) {
+    const session = this.dbClient.getSession();
+    try {
+      otherDevicesId.forEach(async (deviceId) => {
+        await session.writeTransaction((tx) =>
+          tx.run(
+            `MATCH (source:DEVICE {id: $sourceId}), (destination:DEVICE {id: $deviceId}) CREATE (source)-[r:WORKS_WITH {taskId: $taskId}]->(destination)`,
+            {
+              sourceId,
+              deviceId,
+              taskId,
+            }
+          )
+        );
+      });
+    } catch (err) {
+      console.error("Neo4j mesh error", err);
+    } finally {
+      // session.close();
+    }
+  }
 
-    public async getNIdleDevices(n: number): Promise<Device[]> {
-        const session = this.dbClient.getSession();
-        let devices: Device[] = [];
+  public async setStatus(key: string, value: DeviceStatus) {
+    try {
+      await this.client.set(`${key}`, `${value}`);
+    } catch (err) {
+      console.error(`Couldn't set value of ${key} to redis: `, err);
+    }
+  }
 
-        try {
-            const res = await session.readTransaction(tx => 
-                tx.run("MATCH (d:DEVICE) WHERE d.status = \"idle\" RETURN d LIMIT $n", { n })    
-            );
+  public async getStatus(key: string): Promise<DeviceStatus | null> {
+    try {
+      return (await this.client.get(key)) as DeviceStatus;
+    } catch (err) {
+      console.error(`Couldn't get value of ${key} from redis: `, err);
+      return null;
+    }
+  }
 
-            devices = res.records.map(record => ({
-                ...record.get('d').properties,
-            }))
-        } catch (err) {
-            console.error("Neo4j Read error: ", err);
-        }
+  public async getNIdleDevices(n: number): Promise<Device[]> {
+    const session = this.dbClient.getSession();
+    let devices: Device[] = [];
 
-        return devices;
+    try {
+      const res = await session.readTransaction((tx) => tx.run('MATCH (d:DEVICE) WHERE d.status = "idle" RETURN d LIMIT $n', { n }));
+
+      devices = res.records.map((record) => ({
+        ...record.get("d").properties,
+      }));
+    } catch (err) {
+      console.error("Neo4j Read error: ", err);
     }
 
-    public setSocket(id: string, socket: WebSocket | null): void
-	{
-		this.socketStore.set(id, socket);
-	}
+    return devices;
+  }
 
-	public getSocket(id: string): WebSocket | null | undefined
-	{
-		return this.socketStore.get(id);
-	}
+  public setSocket(id: string, socket: WebSocket | null): void {
+    this.socketStore.set(id, socket);
+  }
 
-    public disconnectDevice(deviceId: string) {
-        this.updateDevice({
-            id: deviceId,
-            status: "disconnected",
-        });
-        this.setStatus(deviceId, "disconnected");
-        this.setSocket(deviceId, null);
-    }
+  public getSocket(id: string): WebSocket | null | undefined {
+    return this.socketStore.get(id);
+  }
+
+  public disconnectDevice(deviceId: string) {
+    this.updateDevice({
+      id: deviceId,
+      status: "disconnected",
+    });
+    this.setStatus(deviceId, "disconnected");
+    this.setSocket(deviceId, null);
+  }
 }
--- src/common/TaskRepository.ts
+++ src/common/TaskRepository.ts
@@ -1,69 +1,60 @@
-import { DBClient } from './db';
-import { Singleton } from './Singleton';
+import { DBClient } from "./db";
+import { Singleton } from "./Singleton";
 export interface Task {
-    id: string;
-    devices_count: number;
-    params: string;
-    status: "new" | "ongoing" | "succeeded" | "failed";
-    chunkUrl?: string;
-    dataType: string;
-    dataTypeParams: string;
+  id: string;
+  devices_count: number;
+  params: string;
+  status: "new" | "ongoing" | "succeeded" | "failed";
+  chunkUrl?: string;
+  dataType: string;
+  dataTypeParams: string;
 }
 
 @Singleton
 export class TaskRepository {
-    private dbClient: DBClient;
+  private dbClient: DBClient;
 
-    constructor()
-	{
-		this.dbClient = new DBClient();
-	}
+  constructor() {
+    this.dbClient = new DBClient();
+  }
 
-    public async addTask(task: Task)
-	{
-		const session = this.dbClient.getSession();
-		try
-		{
-			await session.writeTransaction(tx => tx.run("CREATE (newTask:TASK) SET newTask.id = $id, newTask.devices_count = $devices_count, newTask.params = $params, newTask.status = $status", task));
-		} catch (err)
-		{
-			console.error("Neo4J store error", err);
-		} finally
-		{
-			console.log("task stored successfully");
-			session.close();
-		}
-	}
-
-	public async updateTask(task: Task)
-	{
-		const session = this.dbClient.getSession();
-		try
-		{
-			await session.writeTransaction(tx => tx.run("MATCH (t:TASK {id: $id}) SET t.status = $status", task));
-		} catch (err)
-		{
-			console.error("Neo4J store error", err);
-		} finally
-		{
-			console.log("task updated successfully");
-			session.close();
-		}
-	}
-
-    public async getMinTask() {
-        const session = this.dbClient.getSession();
-        let task: Task | null = null;
-        try {
-            const res = await session.readTransaction(tx => 
-                tx.run(`MATCH (t:TASK) RETURN t ORDER BY t.devices_count LIMIT 1`)
-            );
-            const rec = res.records[0].get('t');
-            task = { ...rec.properties }
-        } catch (err) {
-            console.error(err)
-        }
-
-        return task;
+  public async addTask(task: Task) {
+    const session = this.dbClient.getSession();
+    try {
+      await session.writeTransaction((tx) =>
+        tx.run("CREATE (newTask:TASK) SET newTask.id = $id, newTask.devices_count = $devices_count, newTask.params = $params, newTask.status = $status", task)
+      );
+    } catch (err) {
+      console.error("Neo4J store error", err);
+    } finally {
+      console.log("task stored successfully");
+      // session.close();
     }
+  }
+
+  public async updateTask(taskId: string, status: "new" | "ongoing" | "succeeded" | "failed") {
+    const session = this.dbClient.getSession();
+    try {
+      await session.writeTransaction((tx) => tx.run("MATCH (t:TASK {id: $taskId}) SET t.status = $status", { taskId, status }));
+    } catch (err) {
+      console.error("Neo4J store error", err);
+    } finally {
+      console.log("task updated successfully");
+      // session.close();
+    }
+  }
+
+  public async getMinTask() {
+    const session = this.dbClient.getSession();
+    let task: Task | null = null;
+    try {
+      const res = await session.readTransaction((tx) => tx.run(`MATCH (t:TASK) RETURN t ORDER BY t.devices_count LIMIT 1`));
+      const rec = res.records[0].get("t");
+      task = { ...rec.properties };
+    } catch (err) {
+      console.error(err);
+    }
+
+    return task;
+  }
 }
